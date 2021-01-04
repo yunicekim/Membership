c886c11cf37cf837c2a36664cfa2ff90794be202
Yunice Kim <yunicekim@gmail.com>
2021-01-03 22:20:03 -0500

    Add AOP

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,21 +1,47 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
 
 @Configuration
 public class SpringConfig {
 
-    @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository());
+//    private final DataSource datasource;
+//
+//    public SpringConfig(DataSource datasource) {
+//        this.datasource = datasource;
+//    }
+    private final MemberRepository memberRepository;
+    //private EntityManager em;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository, EntityManager em) {
+        this.memberRepository = memberRepository;
+        //this.em = em;
     }
 
     @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+    public MemberService memberService(){
+        return new MemberService(memberRepository);
     }
+
+//    @Bean
+//    public TimeTraceAop timeTraceAop(){
+//        return new TimeTraceAop();
+//    }
+//    @Bean
+//    public MemberRepository memberRepository(){
+
+        //return new MemoryMemberRepository();
+        //return new JdbcMemberRepository(datasource);
+//        return new JdbcTemplateMemberRepository(datasource);
+//        return new JpaMemberRepository(em);
+
+//    }
 }
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -5,10 +5,12 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
     private final MemberRepository memberRepository;
@@ -26,7 +28,7 @@ public class MemberService {
         //duplicate name is not acceptable
 //        Optional<Member> result = memberRepository.findByName(member.getName());
 //        result.ifPresent(m -> {
-//            throw new IllegalStateException(("Aleady exists"));
+//            throw new IllegalStateException(("Already exists"));
 //        });
         validateDuplicateMember(member);
 
